406c50dbbc823f0389a755a91cb2434ff9f3caa2
appuler <83124296+appuler@users.noreply.github.com>
2025-09-20 23:58:22 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/scoop-strength-shop/" : "/",
+  base: mode === "production" ? "/cool-gain/" : "/",
   server: {
     host: "::",
     port: 8080,
