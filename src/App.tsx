b68ef5f9bec357e8d6109db36a6ac7481d56a1fe
appuler <83124296+appuler@users.noreply.github.com>
2025-09-21 10:16:42 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.PROD ? "/scoop-strength-shop" : ""}>
+      <BrowserRouter basename={import.meta.env.PROD ? "/cool-gain" : ""}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
